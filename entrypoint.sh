5bdd1ed818d324a443b1f3da5d33c47bb91c4614
Benjamin Turner <benturn@gmail.com>
2023-10-25 17:31:24 -0700

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,8 +59,7 @@ fi
 
 # Attach postgres cluster to the app if specified.
 if [ -n "$INPUT_POSTGRES" ]; then
-  flyctl postgres create --name "$app-db" --fork-from "$INPUT_POSTGRES" --vm-size shared-cpu-1x || true
-  flyctl postgres attach "$app-db" --app "$app" || true
+  flyctl postgres create --name "$app-db" --fork-from "$INPUT_POSTGRES" --vm-size "$INPUT_VM" || true
 fi
 
 # Trigger the deploy of the new version.
